1e2087c4db2dd41d0e1680d7b0efd70b89639d25
awsalghool <awswf2007@gmail.com>
2025-09-30 20:19:18 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom'; // 👈 أضف هذا
+import { HashRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+if (!rootElement) throw new Error("Could not find root element");
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <HashRouter>
       <App />
